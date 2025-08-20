58c82e27ca1c5c918df36e4f7cbc8533685a87ad
donch1150 <boockza1@gmail.com>
2025-08-20 13:45:02 +0700

    add

---
+++ werapong.py
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+ 
+    printf("ป้อนจำนวนที่ 1: ");
+    scanf("%d", &num1);
+
+    printf("ป้อนจำนวนที่ 2: ");
+    scanf("%d", &num2);
+
+
+    printf("ผลบวก: %d\n", num1 + num2);
+
+    return 0;
+}
