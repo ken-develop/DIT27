847f987ba20fe2159841ca92661ad5a0b4d20037
Zanitti99 <Zanitti06@gmail.comm>
2025-08-20 13:53:05 +0700

    sanit

---
+++ sanit_10.py
@@ -0,0 +1,8 @@
+num1 = float(input("กรุณากรอกตัวเลขตัวที่ 1: "))
+num2 = float(input("กรุณากรอกตัวเลขตัวที่ 2: "))
+
+# คำนวณผลบวก
+sum_result = num1 + num2
+
+# แสดงผลลัพธ์
+print(f"ผลบวกของ {num1} และ {num2} คือ {sum_result}")
