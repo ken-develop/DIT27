a00d10fa05319d5c8ec7ad2f84fcb795d315a9c5
deokn1 <deoknqwe1@gamil.com>
2025-08-20 13:58:07 +0700

    phornpavit

---
+++ phornpavit.py
@@ -0,0 +1,11 @@
+# sum_two_numbers.py
+
+# รับค่าจากผู้ใช้
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# คำนวณผลรวม
+sum_result = a + b
+
+# แสดงผลลัพธ์
+print("Sum =", sum_result)
