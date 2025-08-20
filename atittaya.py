8fddecb456f6e72e9a73ec137d1763dd0ccfb453
atittayaz1 <atittxya@gmail.com>
2025-08-20 13:31:45 +0700

    Remove all files except atittaya.py

--- atittaya.py
+++ atittaya.py
@@ -1,1 +1,11 @@
 print('Atittaya Thongthiam')
+
+# รับค่าจากผู้ใช้
+num1 = int(input("กรอกจำนวนเต็มตัวแรก: "))
+num2 = int(input("กรอกจำนวนเต็มตัวที่สอง: "))
+
+# คำนวณผลรวม
+result = num1 + num2
+
+# แสดงผลลัพธ์
+print( num1, "+", num2, "=", result)
