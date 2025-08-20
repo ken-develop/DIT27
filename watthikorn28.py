042a71eb24e20c8a5c80c68cc22e6c85e1618620
watthikorn2077 <watthikornpraserdnok2077@gmail.com>
2025-08-20 13:57:35 +0700

    watthikorn28

---
+++ watthikorn28.py
@@ -0,0 +1,9 @@
+# รับค่าตัวเลขจากผู้ใช้
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# คำนวณผลลัพธ์
+sum_result = num1 + num2
+
+# แสดงผลลัพธ์
+print("The sum of", num1, "and", num2, "is", sum_result)
